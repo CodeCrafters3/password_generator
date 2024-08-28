db3604c6cb7031391591e779915773685a7b7cae
Kopi3 <kuba-kopacz@o2.pl>
2024-08-28 14:59:26 +0200

    Implemented suggestions and refactored code

--- basic_password_generator.py
+++ basic_password_generator.py
@@ -1,47 +1,24 @@
 import random
 import string
 
-try:
-    password_type = int(input("Choose type of password (1 - easy, 2 - medium , 3 - hard): "))
-except ValueError:
-    print("Please enter a number 1,2 or 3.")
-    
-letters = string.ascii_letters
-digits = string.digits
-special_characters = string.punctuation
+def get_password_length():
+    while True:
+        try:
+            length = int(input("Choose length of password:")) 
+            if length > 0:
+                return length
+            else:
+                print("Length must be a positive integer.")
+        except ValueError:
+            print("Please enter a valid integer.")
+        
+password_length = get_password_length()
+all_characters = string.ascii_letters + string.digits + string.punctuation
+password = []
 
-def easy_password():
-    password = []
-    length = 8
-    
-    for x in range(length):
-        random.character = random.choice(letters)
-        password.append(random.character)        
-    return ''.join(password)
+for index in range(password_length):
+    selected_characters = random.choice(all_characters)
+    password.append(selected_characters)
 
-def medium_password():
-    password = []
-    length = 10
-    
-    for x in range(length):
-        random.character = random.choice(letters + digits)
-        password.append(random.character)        
-    return ''.join(password)
-
-def hard_password():
-    password = []
-    length = 12
-    
-    for x in range(length):
-        random.character = random.choice(letters + digits + special_characters)
-        password.append(random.character)        
-    return ''.join(password)
-
-if password_type == 1:
-    print(easy_password())
-elif password_type == 2:
-    print(medium_password())
-elif password_type == 3:
-    print(hard_password())
-else:
-    print("Please enter a number 1,2 or 3...")
+generated_password = "".join(password)
+print(f"Your password: \n {generated_password}")
