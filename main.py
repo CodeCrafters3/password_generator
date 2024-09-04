66533c3cff1ecf4268680c73cbdede733679f6e6
Kopi3 <158350318+Kopi3@users.noreply.github.com>
2024-09-04 20:22:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-import random
-import string
+import basic_password_generator
 
-def get_password_length():
-    while True:
-        try:
-            length = int(input("Choose length of password:")) 
-            if length > 0:
-                return length
-            else:
-                print("Length must be a positive integer.")
-        except ValueError:
-            print("Please enter a valid integer.")
-        
-password_length = get_password_length()
-all_characters = string.ascii_letters + string.digits + string.punctuation
-password = []
+def main():
+    password = basic_password_generator.generated_password
+    print(f"Your generated password: \n{password}")
 
-for index in range(password_length):
-    selected_characters = random.choice(all_characters)
-    password.append(selected_characters)
-
-generated_password = "".join(password)
-print(f"Your password: \n {generated_password}")
+if __name__ == "__main__":
+    main()
